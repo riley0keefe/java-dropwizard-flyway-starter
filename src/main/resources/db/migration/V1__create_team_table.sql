ac34c2ecfe36e69f41350027148a2d4b7e6017f0
Evan-Lowe <evan.lowe@kainos.com>
2025-08-28 11:44:43 -0300

    Added employee table

--- src/main/resources/db/migration/V1__create_team_table.sql
+++ src/main/resources/db/migration/V1__create_team_table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE Team(
 	Team_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `Name` varchar(30) NOT NULL
+    `Name` varchar(25) NOT NULL
 );
--- src/main/resources/db/migration/V2__create_employee_table.sql
+++ src/main/resources/db/migration/V2__create_employee_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Employee(
 	Employee_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	National_id varchar(15) NOT NULL,
-    `Name` varchar(30) NOT NULL,
+    `Name` varchar(25) NOT NULL,
     Salary float NOT NULL,
     Bank_Account_Num varchar(20) NOT NULL,
     Commission_Rate float NOT NULL,
