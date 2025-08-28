69daf3f9d0bcb4e826bc05c26b9851d49faa13ba
Evan-Lowe <evan.lowe@kainos.com>
2025-08-28 15:40:20 -0300

    Added employee table

--- src/main/resources/db/migration/V2__create_employee_table.sql
+++ src/main/resources/db/migration/V2__create_employee_table.sql
@@ -1,10 +1,19 @@
 CREATE TABLE Employee(
-	Employee_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	National_id varchar(15) NOT NULL,
-    `Name` varchar(25) NOT NULL,
-    Salary float NOT NULL,
-    Bank_Account_Num varchar(20) NOT NULL,
-    Commission_Rate float NOT NULL,
-    Team_id int NOT NULL,
-    FOREIGN KEY (Team_id) REFERENCES Team(Team_id)
+	Employee_ID SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	National_ID varchar(16) NOT NULL,
+    `Name` varchar(128) NOT NULL,
+    Salary DECIMAL(10, 2) NOT NULL,
+    Bank_Account_Num varchar(64) NOT NULL,
+    Commission_Rate DECIMAL(5, 2) DEFAULT 0.00,
+    Team_ID int,
+    FOREIGN KEY (Team_ID) REFERENCES Team(Team_ID)
+);
+
+INSERT INTO Employee (National_ID, `Name`, Salary, Bank_Account_Num, 
+	Commission_Rate, Team_ID)
+SELECT '123256289', 'Ryan Murphy', 120000.36, '123abc45', 100, 3 -- Replace with employee info here
+WHERE EXISTS (
+    SELECT * 
+	FROM Employee
+	WHERE Team_ID = 1 AND Employee_ID = 3 -- Enter Employee_ID here
 );
