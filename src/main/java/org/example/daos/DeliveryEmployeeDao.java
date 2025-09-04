f460cd96521eb9f7aface77822d24d707422ec2d
riley0keefe <riley.o'keefe@kainos.com>
2025-09-04 11:51:19 -0300

    Create delivery employee dao

--- src/main/java/org/example/daos/DeliveryEmployeeDao.java
+++ src/main/java/org/example/daos/DeliveryEmployeeDao.java
@@ -1,5 +1,34 @@
 package org.example.daos;
 
-public class DeliveryEmployeeDao {
+import org.example.models.DeliveryEmployeeRequest;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeliveryEmployeeDao {
+    public int createDeliveryEmployee(DeliveryEmployeeRequest employee) throws
+            SQLException {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            String insertStatement = "INSERT INTO DeliveryEmployees "
+                    + "(`name`, `salary`, `bank_number`,"
+                    + " `national_insurance_number`) "
+                    + "VALUES (?, ?, ?, ?);";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(insertStatement, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, employee.getName());
+            preparedStatement.setDouble(2, employee.getSalary());
+            preparedStatement.setString(3, employee.getBankNumber());
+            preparedStatement.setString(4, employee.getNationalInsuranceNumber());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+        }
+        return -1;
+    }
 }
