870919f127d0a1557deef4c959586060bab36dec
riley0keefe <riley.o'keefe@kainos.com>
2025-09-04 15:13:04 -0300

    Update data values for bank number and national insurance number

--- src/main/resources/db/migration/V1__create_delivery_employee_table.sql
+++ src/main/resources/db/migration/V1__create_delivery_employee_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE DeliveryEmployees (
    id INT PRIMARY KEY AUTO_INCREMENT,
    name VARCHAR(64) NOT NULL,
    salary DECIMAL(11, 2) NOT NULL,
-   bank_number VARCHAR(8) UNIQUE NOT NULL,
-   national_insurance_number VARCHAR(8) UNIQUE NOT NULL
+   bank_number VARCHAR(32) UNIQUE NOT NULL,
+   national_insurance_number VARCHAR(32) UNIQUE NOT NULL
 );
