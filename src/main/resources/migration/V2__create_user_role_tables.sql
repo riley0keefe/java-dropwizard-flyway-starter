00457653e569431c9ef3209dd6b571059c86bc66
riley0keefe <riley.o'keefe@kainos.com>
2025-09-11 11:15:48 -0300

    Create V2__create_user_role_tables.sql

---
+++ src/main/resources/migration/V2__create_user_role_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `role` (
+	role_id TINYINT NOT NULL,
+    name varchar(64) NOT NULL,
+    PRIMARY KEY (role_id)
+);
+
+INSERT INTO role(role_id, name) VALUES(1, 'admin');
+INSERT INTO role(role_id, name) VALUES(2, 'hr');
+INSERT INTO role(role_id, name) VALUES(3, 'user');
+
+CREATE TABLE `user` (
+	username varchar(64) NOT NULL,
+    password varchar(64) NOT NULL,
+    role_id TINYINT NOT NULL,
+    PRIMARY KEY (username),
+    FOREIGN KEY (role_id) REFERENCES role(role_id)
+);
+
+INSERT INTO User(username, password, role_id) VALUES ('admin', 'admin', 1);
+INSERT INTO User(username, password, role_id) VALUES ('hr', 'hr', 2);
+INSERT INTO User(username, password, role_id) VALUES ('user', 'user', 3
