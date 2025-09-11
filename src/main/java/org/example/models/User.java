51c645c511c6bd1530246c3441be261e51d0db1f
riley0keefe <riley.o'keefe@kainos.com>
2025-09-11 10:26:45 -0300

    Update User.java

--- src/main/java/org/example/models/User.java
+++ src/main/java/org/example/models/User.java
@@ -1,4 +1,37 @@
 package org.example.models;
 
 public class User {
+    private String username;
+    private String password;
+    int roleId;
+
+    public User(String username, String password, int roleId) {
+        this.username = username;
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
 }
