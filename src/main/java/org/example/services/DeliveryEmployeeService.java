9f2697400f8b306e6988616e4369f0b151e8a9d5
riley0keefe <riley.o'keefe@kainos.com>
2025-09-11 15:00:17 -0300

    Update DeliveryEmployeeService.java

--- src/main/java/org/example/services/DeliveryEmployeeService.java
+++ src/main/java/org/example/services/DeliveryEmployeeService.java
@@ -9,6 +9,11 @@ import org.example.models.DeliveryEmployeeRequest;
 import java.sql.SQLException;
 
 public class DeliveryEmployeeService {
+    private static final int NINE = 9;
+    private static final int FIFTEEN = 15;
+    private static final int EIGHTEEN = 18;
+    private static final int NINETEEN = 19;
+    private static final int FORTY_NINE = 49;
 
     DeliveryEmployeeDao deliveryEmployeeDao;
 
@@ -24,35 +29,38 @@ public class DeliveryEmployeeService {
 
         String nin = deliveryEmployeeRequest.getNationalInsuranceNumber();
         int lenthOfNIN = nin.length();
-        if (lenthOfNIN != 9 && lenthOfNIN != 15 && lenthOfNIN != 19) {
-            throw new RuntimeException("National Insurance Number " +
-                    "must be 9, 15 or 19 characters long.");
+        if (lenthOfNIN != NINE
+                && lenthOfNIN != FIFTEEN
+                && lenthOfNIN != NINETEEN) {
+            throw new RuntimeException("National Insurance Number "
+                    + "must be 9, 15 or 19 characters long.");
         }
         // Canada NIN check
-        if (lenthOfNIN == 9) {
+        if (lenthOfNIN == NINE) {
             if (!nin.matches("\\d+")) {
-                throw new RuntimeException("Canadian National Insurance " +
-                        "Number can be numbers only.");
+                throw new RuntimeException("Canadian National Insurance "
+                        + "Number can be numbers only.");
             }
         }
 
         // China NIN check first 18 digits last can be X or digit
-        if (lenthOfNIN == 19) {
-            if (!nin.substring(0, 18).matches("\\d+")) {
-                throw new RuntimeException("First 18 characters of Chinese " +
-                        "National Insurance Number must be numbers only.");
+        if (lenthOfNIN == NINETEEN) {
+            if (!nin.substring(0, EIGHTEEN).matches("\\d+")) {
+                throw new RuntimeException("First 18 characters of Chinese "
+                        + "National Insurance Number must be numbers only.");
             }
-            if (!nin.substring(18).matches("\\d|X")) {
-                throw new RuntimeException("Last character of Chinese " +
-                        "National Insurance Number must be a number or an X.");
+            if (!nin.substring(EIGHTEEN).matches("\\d|X")) {
+                throw new RuntimeException("Last character of Chinese "
+                        + "National Insurance Number "
+                        + "must be a number or an X.");
             }
         }
 
         // France NIN check numbers only
-        if (lenthOfNIN == 15) {
+        if (lenthOfNIN == FIFTEEN) {
             if (!nin.matches("\\d+")) {
-                throw new RuntimeException("French National Insurance " +
-                        "Number must be numbers only.");
+                throw new RuntimeException("French National Insurance "
+                        + "Number must be numbers only.");
             }
         }
 
@@ -61,21 +69,23 @@ public class DeliveryEmployeeService {
         }
 
         if (deliveryEmployeeRequest.getFirstName().trim().length() < 2) {
-            throw new RuntimeException("First name must be greater than 2 " +
-                    "characters.");
+            throw new RuntimeException("First name must be greater than 2 "
+                    + "characters.");
         }
-        if (deliveryEmployeeRequest.getFirstName().trim().length() > 49) {
-            throw new RuntimeException("First name must be less than " +
-                    "50 characters.");
+        if (deliveryEmployeeRequest.getFirstName()
+                .trim().length() > FORTY_NINE) {
+            throw new RuntimeException("First name must be less than "
+                    + "50 characters.");
         }
 
         if (deliveryEmployeeRequest.getLastName().trim().length() < 2) {
-            throw new RuntimeException("Last name must be greater than " +
-                    "2 characters.");
+            throw new RuntimeException("Last name must be greater than "
+                    + "2 characters.");
         }
-        if (deliveryEmployeeRequest.getLastName().trim().length() > 49) {
-            throw new RuntimeException("First name must be less than " +
-                    "50 characters.");
+        if (deliveryEmployeeRequest.getLastName().trim()
+                .length() > FORTY_NINE) {
+            throw new RuntimeException("First name must be less than "
+                    + "50 characters.");
         }
 
         if (!deliveryEmployeeRequest.getBankAccount().matches("\\d+")) {
